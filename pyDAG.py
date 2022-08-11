1f71c278aa85989b0986018d3bf2407a9e359410
justaguy12345 <ryanberk135@gmail.com>
2022-08-11 16:40:45 -0500

    updating functions

--- pyDAG.py
+++ pyDAG.py
@@ -7,38 +7,57 @@
 #print(array)
 #dictionary = { 1: array  }
 #print(dictionary)
-
+import random
 class DAG:
     i = 0
     x = 0
     y = 0
     data = 0
     array = [x, y, data]
-    dictionary = {}
+    dictionary = { i : array }
 
 dag = DAG()
-
-def update(i, array):
-    dag.array = array
-    #dag.i = i
-    dag.dictionary = { dag.i : dag.array }
-    dag.i = dag.i + 1
 
 def addEdge(x, y, i, data):
     dag.x = x
     dag.y = y
+    if dag.i == 0:
+        i = 1
     dag.i = i
     dag.data = data
     new_edge = [x, y, data]
+    dag.array = new_edge
+    dag.dictionary = { dag.i : dag.array }
+    dag.i = dag.i + 1 
+
+a = 1
+b = 20
+xArray = []
+yArray = []
+dataArray = []
+
+def checkDirection(data, kind):
+    if kind == 'x':
+        for x in xArray:
+            if x == data:
+                data = random.randint(a, b)
+            else:
+                break
+        return data
+
+def loadDAG(limit):
+    for i in range(limit):
+        newX = checkDirection(random.randint(a, b), 'x')
+        newY = random.randint(a, b)
+        newData = random.randint(a, b)
+        xArray.append(newX)
+        yArray.append(newY)
+        dataArray.append(newData)
+        addEdge(newX, newY, dag.i,  newData)
+        print(dag.dictionary)
+        
+loadDAG(5)
 
 
 
-update(dag.i, dag.array)
-print(dag.dictionary)
-addEdge(4, 5)
-print("dagx = ",dag.x)
-print(dag.dictionary)
-update(dag.i, dag.array)
-print(dag.dictionary)
-update(dag.i, dag.array)
-print(dag.dictionary)
+
